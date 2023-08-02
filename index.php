40afd40eb162fe4c3c3f7f5a939a24c03abe53be
Anastahasia <anasthasia.assani@gmail.com>
2023-08-02 08:10:53 +0400

    quasi rien

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <section>
+        <h1>
+            Trouvez votre havre de paix - Explorez notre sélection d'immobilier d'exception dès aujourd'hui !
+        </h1>
+    </section>
+    <section>
+
+    </section>
+<script src="script.js"></script>
+</body>
+
+</html>
